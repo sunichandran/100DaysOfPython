ea858eeb6b924b9cdc2b6380544c5024affc5e36
sunitha chandamarakshan <sunichandran@gmail.com>
2024-02-13 18:04:49 -0500

    Update and rename hangman to hangman.py

---
+++ hangman.py
@@ -0,0 +1,34 @@
+import random
+
+# create a random list of words
+word_list = ["aardvark", "baboon", "camel"]
+
+# choose a ramdom word
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Psst..the word is {chosen_word}. ')
+
+# display "_" for all the letters in the chosen_word
+display = []
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+  display += "_"
+
+# ask the user an input/guess the letter
+
+guess = input("Guess a letter").lower()
+
+# check the guess with the chosen_word. if the guessed letter matches the letter in the chosen_word, print the letter in that position
+
+for position in range(word_length):
+  letter = chosen_word[position]
+  if letter == guess:
+    display[position] = letter
+
+print(display)
+
+
+
+
